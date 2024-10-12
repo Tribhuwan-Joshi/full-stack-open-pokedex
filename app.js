87d07ec3e29a417dcf03892856de7bcc16494089
tjsm <mathincosmos@gmail.com>
2024-10-12 13:32:17 +0530

    set build-script

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ app.use(express.static("dist"));
 app.get("/version", (req, res) => {
   res.send("1"); // change this string to ensure a new version deployed
 });
-app.get("/version", (req, res) => {
-  res.send("1"); // change this string to ensure a new version deployed
-});
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "Build script"
+npm run build;
+npm run start-prod;
 
 # add the commands here
