535f35681c8c7868930f1234e4632161ba02cb3b
tjsm <mathincosmos@gmail.com>
2024-10-13 11:50:50 +0530

    access port 5000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("./main");
-// get the port from env variable
+// get the port from env variable or set it to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`server  started on port ${PORT}`);
 });
